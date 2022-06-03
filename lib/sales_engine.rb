b7efe9dc370ff78db3b940ea044d577348589852
philmarcu <philmark18@gmail.com>
2022-06-03 13:13:21 -0600

    Feat: add anaylst instance to sales engine class

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,13 +1,19 @@
+require_relative './item_collection'
+require_relative './merchant_collection'
+require_relative './sales_analyst'
+
 class SalesEngine
 
-  attr_reader :item_collection, :merchant_collection
+  attr_reader :item_collection,
+              :merchant_collection,
+              :analyst
   def initialize(items_path, merc_path)
     @item_collection = ItemCollection.new(items_path)
     @merchant_collection = MerchantCollection.new(merc_path)
+    @analyst = Analyst.new
   end
 
   def self.from_csv(data)
     SalesEngine.new(data[:items], data[:merchants])
   end
-
 end
