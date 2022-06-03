34254d7b2c6f5b841eee9566068da3f30f0f8ee0
philmarcu <philmark18@gmail.com>
2022-06-03 13:26:40 -0600

    Refactor: change sales engine spec to include analyst class, add before each

--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -4,44 +4,37 @@ require "./lib/merchant_collection"
 require "./lib/sales_analyst"
 
 RSpec.describe SalesEngine do
+  before :each do
+    @se = SalesEngine.from_csv({
+        :items => "./data/items.csv",
+        :merchants => "./data/merchants.csv"
+      })
+  end
 
   it "exists" do
-    sales_engine = SalesEngine.from_csv({
-      :items => "./data/items.csv",
-      :merchants => "./data/merchants.csv"
-    })
-    expect(sales_engine).to be_instance_of SalesEngine
+    expect(@se).to be_instance_of SalesEngine
   end
 
   it "can return an array of all items" do
-    sales_engine = SalesEngine.from_csv({
-      :items => "./data/items.csv",
-      :merchants => "./data/merchants.csv"
-    })
-
-    expect(sales_engine.item_collection).to be_instance_of ItemCollection
+    expect(@se.item_collection).to be_instance_of ItemCollection
   end
 
   it "can return an array of all instances" do
-    sales_engine = SalesEngine.from_csv({
-      :items => "./data/items.csv",
-      :merchants => "./data/merchants.csv"
-    })
-
-    expect(sales_engine.merchant_collection).to be_instance_of MerchantCollection
+    expect(@se.merchant_collection).to be_instance_of MerchantCollection
   end
 
   it 'can return child instances' do
-    se = SalesEngine.from_csv({
-      :items => "./data/items.csv",
-      :merchants => "./data/merchants.csv"
-    })
-
-    ic = se.item_collection
+    ic = @se.item_collection
     item = ic.find_by_name("Thukdokhin wax cord")
 
     expect(ic).to be_a(ItemCollection)
     expect(item.id).to eq("263404585")
+  end
+
+  it 'can have a sales_analyst' do
+    sa = @se.analyst
+    expect(sa).to be_a(Analyst)
+    expect(sa.average_items_per_merchant).to eq(2.88)
   end
 
 end
