e7883b5d5a02452d5c0c8806d093e94f73333703
philmarcu <philmark18@gmail.com>
2022-06-03 13:16:08 -0600

    Feat: create 3 tests for analyst class, 2 passing

--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -1,17 +1,27 @@
 require "./lib/sales_engine"
-require "./lib/item_collection"
-require "./lib/merchant_collection"
+require "item_collection"
+require "merchant_collection"
 require "./lib/sales_analyst"
 
 RSpec.describe Analyst do
+  before :each do
+    @sales_engine = SalesEngine.from_csv({
+            :items => "./data/items.csv",
+            :merchants => "./data/merchants.csv"
+          })
+    @sales_analyst = @sales_engine.analyst
+  end
+
  it 'exist' do
-   sales_analyst = Analyst.new("./data/items.csv", "./data/merchants.csv")
-   expect(sales_analyst).to be_a(Analyst)
+   expect(@sales_analyst).to be_a(Analyst)
  end
 
  it 'calculates average items per merchant' do
-   sales_analyst = Analyst.new("./data/items.csv", "./data/merchants.csv")
-   expect(sales_analyst.average_items_per_merchant).to eq(2.88)
+   expect(@sales_analyst.average_items_per_merchant).to eq(2.88)
+ end
+
+ xit 'can return the average items by standard deviation' do
+   expect(@sales_analyst.average_items_per_merchant_standard_deviation).to eq(3.26)
  end
 
 end
