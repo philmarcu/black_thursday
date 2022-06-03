c647ec108f2d0230fa79eba0827d458de84d7c37
philmarcu <philmark18@gmail.com>
2022-06-03 13:12:28 -0600

    Refactor: change analyst class to connect to sales engine class

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,19 +1,24 @@
 require 'CSV'
-require './lib/item'
-require './lib/merchant'
-require './lib/sales_engine'
+require_relative 'item_collection'
+require_relative 'merchant_collection'
+require_relative 'sales_engine'
 
-class Analyst < SalesEngine
-  def initialize(items_path, merc_path)
-    super
-    @ic = @item_collection.all
-    @mc = @merchant_collection.all
+class Analyst
+
+  attr_reader :ic, :mc
+  def initialize
+    @ic = ItemCollection.new("./data/items.csv")
+    @mc = MerchantCollection.new("./data/merchants.csv")
   end
 
   def average_items_per_merchant
-    item_total = @ic.length
-    merc_total = @mc.length
+    item_total = @ic.all.length
+    merc_total = @mc.all.length
     (item_total / merc_total.to_f).round(2)
+  end
+
+  def average_items_per_merchant_standard_deviation
+    mean = average_items_per_merchant
   end
 
 end
