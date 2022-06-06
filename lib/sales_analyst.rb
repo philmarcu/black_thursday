4142ad0b8d3cb031127bfa0924817388e05cb54c
philmarcu <philmark18@gmail.com>
2022-06-06 10:06:09 -0600

    Feat: add new standard deviation + avg avg price method

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -2,14 +2,16 @@ require 'CSV'
 require 'bigdecimal'
 require_relative 'item_collection'
 require_relative 'merchant_collection'
+require_relative 'invoice_collection'
 require_relative 'sales_engine'
 
 class Analyst
 
-  attr_reader :ic, :mc
+  attr_reader :ic, :mc, :inv_c
   def initialize
     @ic = ItemCollection.new("./data/items.csv")
     @mc = MerchantCollection.new("./data/merchants.csv")
+    @inv_c = InvoiceCollection.new("./data/invoices.csv")
   end
 
   def average_items_per_merchant
@@ -26,7 +28,7 @@ class Analyst
 
   def square_diffs_of_total_items
     mean = average_items_per_merchant
-    total_items_per_merchant.map {|item| square = (item - mean) ** 2}
+    total_items_per_merchant.map {|item| (item - mean) ** 2}
   end
 
   def sum_of_total_item_nums
@@ -51,8 +53,7 @@ class Analyst
   end
 
   def merchants_with_high_item_count
-    merc_total = @mc.all
-    merc_total.find_all do |merchant|
+    @mc.all.find_all do |merchant|
       items = @ic.find_all_by_merchant_id(merchant.id)
       items.count > one_std_dev_above_merchant_std_dev
     end
@@ -85,7 +86,59 @@ class Analyst
     sum = sum_of_unit_prices(merchant_id)
     count = item_count_for_merchant(merchant_id)
     final_output = (sum / count.to_f)
-    avg = (sum / count.to_f).to_i.size
-    BigDecimal(final_output, avg)
+    avg = (sum / count)
+    # BigDecimal(final_output, avg)
+  end
+
+  def collect_average_item_prices
+    avg_price_arr = []
+    @mc.all.each do |merchant|
+      avg_price_arr << average_item_price_for_merchant(merchant.id)
+    end
+    avg_price_arr
+  end
+
+  def avg_price_all
+    total = @ic.all.map {|item| item.unit_price}
+    sum = total.sum {|price| price.to_i}
+    avg = (sum / @ic.all.size.to_f)
+  end
+
+  def square_price_diffs
+    total = @ic.all.map {|item| item.unit_price}
+    mean = avg_price_all
+    total.map {|price| (price.to_i - mean) ** 2 }
+  end
+
+  def sum_of_total_price_diffs
+    square_price_diffs.sum {|price| price}
+  end
+
+  def price_variance
+    sum = sum_of_total_price_diffs
+    variance = (sum / (@ic.all.size.to_f - 1))
+
+  end
+
+  def std_dev_of_prices_per_item
+    variance = price_variance
+    std_dev = (Math.sqrt(variance).round(2) / 10)
+   end
+
+  def average_average_price_per_merchant
+    merc_total = @mc.all.size
+    sum = collect_average_item_prices.sum
+    final_output = ((sum / merc_total) / 100).to_f
+    # BigDecimal(final_output) .round(2).to_s("F").to_f
+  end
+
+  def two_std_dev_above_prices_per_item
+    mean = avg_price_all
+    std_dev = std_dev_of_prices_per_item
+    (((mean + std_dev) * 2).to_f).round(2)
+  end
+
+  def golden_items
+
   end
 end
--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -59,6 +59,41 @@ RSpec.describe Analyst do
  it 'returns the average item price for merchant' do
    expect(@sales_analyst.average_item_price_for_merchant("12336965")).to eq(59910)
    expect(@sales_analyst.average_item_price_for_merchant("12334159")).to eq(3150)
-   expect(@sales_analyst.average_item_price_for_merchant("12334159").class).to be(BigDecimal)
+ end
+
+ it 'can collect the average item prices for all merchants' do
+   expect(@sales_analyst.collect_average_item_prices).to be_a(Array)
+ end
+
+ xit 'returns the average average price per merchant' do
+   expect(@sales_analyst.average_average_price_per_merchant).to eq(350.29)
+ end
+
+ xit 'returns the avg price for all merchants' do
+   expect(@sales_analyst.avg_price_all).to eq(25105.51)
+ end
+
+ it 'returns the square price diff' do
+   expect(@sales_analyst.square_price_diffs).to be_a(Array)
+ end
+
+ it 'returns the sum of price differences' do
+   expect(@sales_analyst.sum_of_total_price_diffs).to eq(114959048897813.61)
+ end
+
+ xit 'can return variance of prices' do
+   expect(@sales_analyst.price_variance).to eq(84157429647.0085)
+ end
+
+ xit 'returns the std of prices per item' do
+   expect(@sales_analyst.std_dev_of_prices_per_item).to eq(29009.9)
+ end
+
+ xit 'returns two standard deviations above prices per' do
+   expect(@sales_analyst.two_std_dev_above_prices_per_item).to eq(108230.82)
+ end
+
+ xit 'can return the golden_items' do
+   expect(@sales_analyst.golden_items.length).to eq(5)
  end
 end
